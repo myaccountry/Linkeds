1993a3d6ffa8a9078b3f91b880bf9263ef887bb1
ZagadochniyManchik <eqorrannev@gmail.com>
2023-11-22 22:49:47 +0400

    Add data to config files

---
+++ CONFIG/logs_config.py
@@ -0,0 +1,7 @@
+GREY = "\x1b[1;37m"
+BLUE = "\x1b[1;34m"
+YELLOW = "\x1b[33;20m"
+RED = "\x1b[31;20m"
+BOLD_RED = "\x1b[41m"
+RESET = "\x1b[0m"
+LOGGER_FORMAT = "[%(asctime)s] [%(name)s] [%(levelname)s] -> %(message)s"
