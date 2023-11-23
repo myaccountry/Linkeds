ef7a34ac8bb10d4cef2edcbe46311f34ce175779
ZagadochniyManchik <eqorrannev@gmail.com>
2023-11-23 22:15:20 +0400

    feat: Add CONFIG data

--- CONFIG/database_config.py
+++ CONFIG/database_config.py
@@ -1,2 +1,8 @@
 
+DB_HOST = '127.0.0.1'
+DB_PORT = 3306
+DB_NAME = 'LINKEDS_DB'
+DB_USER = 'mysql'
+DB_PASSWORD = 'mysql'
+DB_CHARSET = 'utf8mb4'
 
--- CONFIG/logs_config.py
+++ CONFIG/logs_config.py
@@ -5,3 +5,5 @@ RED = "\x1b[31;20m"
 BOLD_RED = "\x1b[41m"
 RESET = "\x1b[0m"
 LOGGER_FORMAT = "[%(asctime)s] [%(name)s] [%(levelname)s] -> %(message)s"
+
+LOGS_PATH = "D:/PythonFiles/Rannev_Python/Linkeds/LOGS"
